64b4a8addd393636d4864b7fd41fc13d5375abd2
Trystan1 <trystan.jones@altran.com>
2021-09-23 13:44:33 +0100

    initial commit

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from game import Game
 from paperbot import PaperBot
 from rockbot import RockBot
 
+
 def main():
+  # we are testing the commit here
   bot1 = PaperBot()
   bot2 = RockBot()
   game = Game(bot1, bot2)
   result = game.play()
   pprint(result)
+
 
 if __name__ == '__main__':
   main()
